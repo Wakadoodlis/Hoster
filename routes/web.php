4ebf2267dd5af837253bd8d2328b59b2cfca73c0
Martynas <marcius117@gmail.com>
2018-09-18 13:32:43 +0300

    first steps

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,8 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
-
+ */
+//  Uzkrauna home page - base.blade.php faila
 Route::get('/', function () {
-    return view('welcome');
+    return view('base');
 });
