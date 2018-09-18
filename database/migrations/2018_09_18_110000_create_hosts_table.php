0462dfaacfefcf7582672c8cfcd7ee79c74aaf55
Martynas <marcius117@gmail.com>
2018-09-18 14:42:00 +0300

    creating DB,dummy data

---
+++ database/migrations/2018_09_18_110000_create_hosts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hosts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('provider');
+            $table->mediumText('product');
+            $table->mediumText('plan');
+            $table->integer('price');
+            $table->mediumText('price_url');
+            $table->mediumText('price_parser');
+            $table->mediumText('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hosts');
+    }
+}
