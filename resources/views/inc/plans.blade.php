1e5803fabda2a90e79f72a3121ee515cc673cf45
Martynas <marcius117@gmail.com>
2018-09-20 21:53:16 +0300

    adjustment

--- resources/views/inc/plans.blade.php
+++ resources/views/inc/plans.blade.php
@@ -3,47 +3,48 @@
     <h2 class="h2">Exsisting plans</h2>
 </div>
 @include('inc.messages') {{-- FORM VALIDACIJOS ZINUTES--}}
-<table class="table">
-    <thead>
-      <tr> 
-        {{-- <th scope="col">#</th> --}}
-        <th scope="col">ID</th>
-        <th scope="col">Provider</th>
-        <th scope="col">Product</th>
-        <th scope="col">Plan</th>
-        <th scope="col">Normal Price</th>
-        <th scope="col">Sale Price</th>
-      </tr>
-    </thead>
-    <tbody>
-        {{-- HOSTS ATVAIZDAVIMAS IS DB--}}   
-        @if(count($hosts) > 0) {{-- TIKRINIMAS JEI DB NEBUS HOSTU--}} 
-            @foreach($hosts as $host)
-            <tr scope="row">
-                <th>{{$host->id}}</th>
-                <th> <a href="/hosts/{{$host->id}}">{{$host->provider}} </a></th>
-                <th>{{$host->product}}</th>
-                <th>{{$host->plan}}</th>
-                <th>{{$host->price}} $ </th>
-            </tr>
-            @endforeach
-        @else
-            <p> No hosts found </p>
-        @endif 
-         
-    </tbody>
-
-</table>
-
+<div class="row">
+    <div class="col-md-7">
+        <table class="table">
+            <thead>
+                 <tr> 
+                    {{-- <th scope="col">#</th> --}}
+                    <th scope="col">ID</th>
+                    <th scope="col">Provider</th>
+                    <th scope="col">Product</th>
+                    <th scope="col">Plan</th>
+                    <th scope="col">Price</th>
+                    {{-- <th scope="col">Sale Price</th> --}}
+                </tr>
+            </thead>
+             <tbody>
+                 {{-- HOSTS ATVAIZDAVIMAS IS DB--}}   
+                @if(count($hosts) > 0) {{-- TIKRINIMAS JEI DB NEBUS HOSTU--}} 
+                    @foreach($hosts as $host)
+                        <tr scope="row">
+                            <th>{{$host->id}}</th>
+                            <th> <a href="/hosts/{{$host->id}}">{{$host->provider}} </a></th>
+                            <th>{{$host->product}}</th>
+                            <th>{{$host->plan}}</th>
+                            <th>{{$host->price}} $ </th>
+                        </tr>
+                    @endforeach
+                @else
+                        <p> No hosts found </p>
+                @endif 
+              </tbody>
+            </table>
+        </div>
 {{-- VISOS KAINOS IS https://hosting.review/best-web-hosting/ --}}
-
-<h1>All host prices:</h1>
-    <ul>
-         @foreach($prices as $price)
-         <li> {{$price}} </li>
-         @endforeach
-    </ul>
-        
+    <div class="col-md-4">
+            <h2 class="mb-4">All host plan prices:</h2>
+                <ul class="plan-price">
+                    @foreach($prices as $price)
+                    <p> Price: {{$price}} <p>
+                    @endforeach
+                </ul>
+    </div>
+</div>     {{-- END ROW DIV --}}    
       
 
 
